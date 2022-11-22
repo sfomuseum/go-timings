d00e01730465121d5366409f5597556d99b65751
sfomuseumbot <sfomuseumbot@localhost>
2022-11-22 14:44:38 -0800

    snapshot: basic working since timing monitor

---
+++ since_test.go
@@ -0,0 +1,65 @@
+package timings
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestSinceMonitor(t *testing.T) {
+
+	ctx := context.Background()
+
+	m, err := NewMonitor(ctx, "since://")
+
+	if err != nil {
+		t.Fatalf("Failed to create monitor, %v", err)
+	}
+
+	err = m.Start(ctx, os.Stdout)
+
+	if err != nil {
+		t.Fatalf("Failed to start monitor, %v", err)
+	}
+
+	done_ch := make(chan bool)
+
+	ticker := time.NewTicker(time.Second * 2)
+	after := time.After(10 * time.Second)
+
+	var signal_err error
+
+	go func() {
+
+		for {
+			select {
+			case <-ticker.C:
+
+				err = m.Signal(ctx)
+
+				if err != nil {
+					signal_err = err
+					done_ch <- true
+					return
+				}
+
+			case <-after:
+				done_ch <- true
+				return
+			}
+		}
+	}()
+
+	<-done_ch
+
+	if signal_err != nil {
+		t.Fatalf("There was a problem signaling the monitor, %v", signal_err)
+	}
+
+	err = m.Stop(ctx)
+
+	if err != nil {
+		t.Fatalf("Failed to stop monitor, %v", err)
+	}
+}
